cadfef0c52ac70cd91339932761f688d4c81b7d7
Rami Enbashi <rami.enbashi@datadoghq.com>
2020-11-04 15:02:40 -0500

    Remove dist and add more capabilities

--- src/constants.ts
+++ src/constants.ts
@@ -7,11 +7,15 @@ export enum UiAppCapabilityType {
     APP_CONTEXT = 'app_context',
     DASHBOARD_COG_MENU = 'dashboard_cog_menu',
     DASHBOARD_CUSTOM_WIDGET = 'dashboard_custom_widget',
+    DASHBOARD_PAGE_CONTEXT = 'dashboard_page_context',
     APP_ROUTING = 'app_routing'
 }
 
 export enum UiAppEventToSubscribeType {
-    DASHBOARD_COG_MENU_CONTEXT = 'dashboard_cog_menu_context'
+    DASHBOARD_COG_MENU_CONTEXT = 'dashboard_cog_menu_context',
+    DASHBOARD_TIMEFRAME_CHANGE = 'dashboard_timeframe_change',
+    DASHBOARD_TEMPLATE_VAR_CHANGE = 'dashboard_template_var_change',
+    DASHBOARD_CUSTOM_WIDGET_OPTIONS_CHANGE = 'dashboard_custom_widget_options_change'
 }
 export enum UiAppEventToTriggerType {
     RELOAD_FRAME = 'reload_frame',
