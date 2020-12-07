8f85212916559822177cd61e19242f234ff1975f
david.leonard <david.leonard@datadoghq.com>
2020-12-07 16:43:44 -0500

    Add dashboard context menu feature

--- src/features/index.ts
+++ src/features/index.ts
@@ -1,10 +1,12 @@
 import { dashboardCogMenu } from './dashboard-cog-menu';
+import { dashboardContextMenu } from './dashboard-context-menu';
 import { dashboardCustomWidget } from './dashboard-custom-widget';
 import { modals } from './modals';
 import { sidePanels } from './side-panels';
 
 export const features = [
     dashboardCogMenu,
+    dashboardContextMenu,
     dashboardCustomWidget,
     modals,
     sidePanels
