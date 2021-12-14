fa26a7a45075f597afe4747b894022313d0073e0
tdimnet <thomas.dimnet@datadoghq.com>
2021-12-14 18:17:34 +0100

    Update react cod

--- examples/random-dog/dog-image-widget/src/widget/index.tsx
+++ examples/random-dog/dog-image-widget/src/widget/index.tsx
@@ -1,95 +1,83 @@
 import {
-  WidgetOptionItemType,
-  init,
-  EventType,
+    WidgetOptionItemType,
+    init,
+    EventType,
 } from "@datadog/ui-extensions-sdk";
-import "./../index.css";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 
 import "./widget.css";
+import "./../index.css";
 import "typeface-roboto";
 import "milligram";
-import { useEffect, useState } from "react";
 
 const client = init();
 
 function Widget() {
-  const [image, setImage] = useState("");
+    const [ breeds, setBreeds ] = useState([])
+    const [ image, setImage ] = useState(null);
 
-  useEffect(() => {
-    console.log("running");
+    useEffect(() => {
+            fetch("http://localhost:3001/breeds")
+                .then(res => res.json())
+                .then(({ breeds }) => setBreeds(breeds))
+                .catch(err => console.log('An error occurs on fetching breeds', err))
+    }, [])
 
-    client.events.on(
-      EventType.DASHBOARD_CUSTOM_WIDGET_OPTIONS_CHANGE,
-      async ({ breed }) => {
-        if (typeof breed !== "string") {
-          return;
-        }
-        getImage(breed);
-      }
-    );
-  }, []);
+    useEffect(() => {
+            if (breeds.length) {
+                const options = breeds.map(breed => ({
+                    label: breed['name'],
+                    value: breed['id']
+                }))
 
-  async function cycleImage() {
-    const breed = await client.getContext().then((c) => {
-      return c.widget?.definition.options?.breed;
-    });
-    getImage(breed);
-  }
-
-  async function getImage(breed: string) {
-    const imageUrl = await fetch(
-      `http://localhost:3001/image?breed_id=${breed}`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        return data.imageUrl;
-      });
-    console.log(imageUrl);
-    setImage(imageUrl);
-  }
-
-  var imageElement;
-
-  if (image) {
-    imageElement = React.createElement("img", { src: image }, null);
-  }
-
-  fetch("http://localhost:3001/breeds")
-    .then((response) => response.json())
-    .then((data) => {
-      var options = [{ label: "All Breeds", value: "0" }];
-      data.breeds.forEach((breed: { name: string; id: number }) => {
-        options.push({ label: breed.name, value: breed.id.toString() });
-      });
-
-      client.dashboard.customWidget.updateOptions([
-        {
-          type: WidgetOptionItemType.STRING,
-          name: "breed",
-          label: "Select a Dog Breed to see Random Images of",
-          enum: options,
-          order: 1,
+                client.dashboard.customWidget.updateOptions([
+                    {
+                        type: WidgetOptionItemType.STRING,
+                        name: 'breed',
+                        label: 'Select a Dog Breed so see Random Images of',
+                        enum: options,
+                        order: 1
+                    }
+                ])
+            }
         },
-      ]);
-    });
+    [breeds])
 
-  return (
-    <div>
-      <div id="button-wrapper">
-        <button id="new-dog" onClick={cycleImage}>
-          New Dog
-        </button>
-      </div>
-      <div className="image-wrapper">{imageElement}</div>
-    </div>
-  );
+    useEffect(() => {
+        client.events.on(
+            EventType.DASHBOARD_CUSTOM_WIDGET_OPTIONS_CHANGE,
+            () => console.log('===')
+        )
+    }, [])
+
+    const cycleImage = async () => {}
+
+    const getImage = async () => {}
+
+    return (
+        <div>
+            <div className="button-wrapper">
+                <button className="new-dog" onClick={() => console.log('click')}>
+                    New Dog
+                </button>
+            </div>
+            {
+                image && (
+                    <div className="image-wrapper">
+                        <img src={image} alt="" />
+                    </div>
+                )
+            }
+        </div>
+    )
 }
 
 export default function render() {
-  ReactDOM.render(
-    <React.StrictMode>{<Widget />}</React.StrictMode>,
-    document.getElementById("root")
-  );
+    ReactDOM.render(
+        <React.StrictMode>
+            <Widget />
+        </React.StrictMode>,
+        document.getElementById("root")
+    );
 }
