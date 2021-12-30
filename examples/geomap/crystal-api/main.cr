80509f5417e23514bf6700b9f1398407dd89aed8
tdimnet <thomas.dimnet@datadoghq.com>
2021-12-30 15:26:34 +0100

    Add the Crystal API

---
+++ examples/geomap/crystal-api/main.cr
@@ -0,0 +1,63 @@
+require "http/server"
+require "json"
+
+bind = ENV["BIND"] ||= "0.0.0.0"
+port = ENV["PORT"] ||= "8080"
+port = port.to_i
+
+
+server = HTTP::Server.new do |context|
+    ## Process Context
+    req = context.request
+    headers = req.headers
+    path = req.path
+    params = req.query_params.to_h
+    method = req.method
+
+    # Conditional request body handling
+    if method == "POST"
+        if headers["Content-Type"] == "application/json"
+            body = JSON.parse(req.body.as(IO).gets_to_end.to_s)
+        else
+            body = req.body.as(IO).gets_to_end.to_s
+        end
+    else
+        body = nil
+    end
+
+    ## Get geolookup ip either from x-forwarded-for header or from url query params
+    if headers.has_key?("X-Forwarded-For") ; ip = headers["X-Forwarded-For"] ; end
+    if headers.has_key?("ip") ; ip = headers["ip"] ; end
+
+    ## Run GeoLookup
+    if ip.nil?
+        geo = HTTP::Client.get "https://ifconfig.co/json"
+    end
+
+    if ! geo.nil?
+        geo_j = JSON.parse(geo.body)
+        geo_h = geo_j.as_h
+    end
+
+    ## Build response object
+    response = {
+        "http" => {
+            "headers": headers,
+            "method": method,
+            "path": path,
+            "params": params
+        },
+        "geo" => geo_h
+    }
+
+    context.response.content_type = "text/json"
+    context.response.print response.to_pretty_json
+    context.response.status_code = 200
+    context.response.close
+end
+
+address = server.bind_tcp bind, port
+puts "Magic happens on port #{port}"
+
+server.listen
+
