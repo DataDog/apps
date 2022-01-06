40ba47793c148f002f3f67d80a9b188fac702df4
tdimnet <thomas.dimnet@datadoghq.com>
2022-01-06 16:11:25 +0100

    Create the dashboard and widgets

--- examples/geomap/setup/index.js
+++ examples/geomap/setup/index.js
@@ -5,6 +5,7 @@ const {
     DD_SITE
 } = require('./constants')
 const createApp = require('./createDatadogApp')
+const createDashboard = require('./createDatadogDashboard')
 
 async function main() {
     console.log('Configuring your account')
@@ -16,6 +17,11 @@ async function main() {
 
     console.log('Creating and configuring the Datadog App')
     const appId = await createApp()
+
+    console.log('Creating the dashboard and adding the different widgets')
+    await createDashboard(configuration, appId)
+
+    console.log(`You can now go to ${BASE_URL}`)
 }
 
 main()
