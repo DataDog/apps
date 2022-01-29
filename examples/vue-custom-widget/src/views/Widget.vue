352734ed78adf65c542d71625fbdf9bbe7ddb6b4
Hardy Jones <88002054+joneshf-dd@users.noreply.github.com>
2022-01-28 17:17:24 -0800

    Hook up the metric option
    We make this custom widget behave more like the starter-kit.
    
    We follow much the same pattern, grab the initial value, setup an event
    listener, render the metric. We also clean up the listener when we're
    done. This is based off of the lifecycle diagram:
    https://v3.vuejs.org/guide/instance.html#lifecycle-diagram. If there's a
    more idiomatic way to do this, we can switch to that instead.

--- examples/vue-custom-widget/src/views/Widget.vue
+++ examples/vue-custom-widget/src/views/Widget.vue
@@ -2,19 +2,46 @@
     <section>
         <h2>Hello App Developer! 👋</h2>
         <p>Welcome to your first Datadog application.</p>
-        <p>Your favorite metric is: <strong>TODO</strong></p>
+        <p>
+            Your favorite metric is: <strong>{{ metric }}</strong>
+        </p>
     </section>
 </template>
 
 <script>
-import { init } from '@datadog/ui-extensions-sdk';
+import { EventType, init } from '@datadog/ui-extensions-sdk';
 import 'milligram';
 import 'typeface-roboto';
 
-init();
+const client = init();
 
 export default {
-    name: 'Widget'
+    name: 'Widget',
+    data() {
+        return {
+            metric: 'system.cpu.idle',
+            $_unsubscribe: undefined
+        };
+    },
+    mounted() {
+        client.getContext().then(context => {
+            this.metric = context.widget?.definition.options?.metric;
+        });
+
+        this.$_unsubscribe = client.events.on(
+            EventType.DASHBOARD_CUSTOM_WIDGET_OPTIONS_CHANGE,
+            options => {
+                if (typeof options.metric !== 'string') {
+                    return;
+                }
+
+                this.metric = options.metric;
+            }
+        );
+    },
+    beforeUnmount() {
+        this.$_unsubscribe?.();
+    }
 };
 </script>
 
