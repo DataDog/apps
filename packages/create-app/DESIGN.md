eeec3e476d3eb6abc25c92619c6b5082d9372f39
Hardy Jones <88002054+joneshf-dd@users.noreply.github.com>
2022-02-02 12:15:57 -0800

    UIAPPS-93 Setup template for `--features` option
    We copy most of the code in the `starter-kit` example.
    
    For the most part what we're doing is taking the `starter-kit` example
    and allowing App developers to choose only the parts they want.
    
    The way this works is explained more in the `DESIGN.md` file. But for
    the sake of the commit, we're generating whatever is given in the option
    and only what is given in the option. We use handlebars as the
    templating language to generate the the files, but only where necessary.
    
    We'll want to expand on this more as we get more feedback. For now, this
    is the first pass at having *something* that we can build on top of.

--- packages/create-app/DESIGN.md
+++ packages/create-app/DESIGN.md
@@ -2,11 +2,25 @@
 
 ## Initialize Command
 
+There are two ways to use this command: with [examples](#examples) and with [features](#features).
+
+### Examples
+
 The current model this package works off of is that there's an `examples` directory at the top-level of this repo.
 With this `examples` directory, we should be able to initialize with any of the actual examples.
 This package downloads a tarball of the repo, extracts it, then copies only the specific example into the App developer's file system.
 Once the example is on disk, it runs either `npm install` or `yarn install` depending on how it's called:
 `npm init` gives `npm install`, `yarn create` gives `yarn install`.
+
+### Features
+
+There is a `src/template` directory in this package that is used to generate the individual features.
+The majority of the files are used directly since there is a general structure necessary to run an App.
+In some of these files, we use [handlebars][] to generate the correct code.
+The features that are given and used to only copy over the necessary files and generate the necessary code.
+For example,
+`yarn create @datadog/app --features dashboard-cog-menu,side-panel` would generate all the normal scaffolding and also only generate files specific to the dashboard cog menu and side panel features.
+Whereas `yarn create @datadog/app --features custom-widget` would generate all the normal scaffolding and also only generate files specific to the custom widget feature.
 
 ## Build
 
@@ -18,3 +32,5 @@ Without this approach, all of the runtime dependencies would have to be installe
 That said, if we find that this approach doesn't work us for some reason (build process issues, too abrasive in theory, etc.),
 we can always move to something else.
 It's not integral to this package that it work this way, it's just currently an easy thing to do.
+
+[handlebars]: https://handlebarsjs.com/
--- packages/create-app/src/initialize.ts
+++ packages/create-app/src/initialize.ts
@@ -1,6 +1,8 @@
 import * as clipanion from 'clipanion';
 import * as fs from 'fs';
 import * as got from 'got';
+import * as handlebars from 'handlebars';
+import * as path from 'path';
 import * as pkgInstall from 'pkg-install';
 import * as stream from 'stream';
 import * as tar from 'tar';
@@ -8,6 +10,17 @@ import * as typanion from 'typanion';
 import * as util from 'util';
 
 const pipeline = util.promisify(stream.Stream.pipeline);
+
+/**
+ * The configuration needed to initialize the correct features.
+ */
+type FeaturesConfig = {
+    customWidget: boolean;
+    dashboardCogMenu: boolean;
+    modal: boolean;
+    sidePanel: boolean;
+    widgetContextMenu: boolean;
+};
 
 const defaultBranch = 'master';
 const defaultExample = 'starter-kit';
@@ -19,6 +32,74 @@ const features = [
     'side-panel',
     'widget-context-menu'
 ] as const;
+
+const templateDirectory: string = path.join(__dirname, 'template');
+
+/**
+ * Copies a file from the {@link templateDirectory} to the {@link directory}.
+ * Respects the {@link pathInTemplateDirectory}.
+ *
+ * E.g. If the {@link directory} is `new-app`,
+ * and the {@link pathInTemplateDirectory} is `foo/bar/baz.ts`,
+ * it will copy the file from `template/foo/bar/baz.ts` to `new-app/foo/bar/baz.ts`.
+ *
+ * @param directory The directory to copy to.
+ * @param pathInTemplateDirectory The file in the {@link templateDirectory} to copy from.
+ */
+async function copyFromTemplate(
+    directory: string,
+    pathInTemplateDirectory: string
+): Promise<void> {
+    const source: string = path.join(
+        templateDirectory,
+        pathInTemplateDirectory
+    );
+    const destination: string = path.join(directory, pathInTemplateDirectory);
+    const destinationDirectory: string = path.dirname(destination);
+
+    await fs.promises.mkdir(destinationDirectory, { recursive: true });
+    return fs.promises.copyFile(source, destination);
+}
+
+/**
+ * Copies a handlebars file from the {@link templateDirectory} to the {@link directory} using the given {@link config}.
+ * Respects the {@link pathInTemplateDirectory}.
+ * Will trim the `.handlebars` extension from the {@link pathInTemplateDirectory}.
+ *
+ * E.g. If the {@link directory} is `new-app`,
+ * and the {@link pathInTemplateDirectory} is `foo/bar/baz.ts.handlebars`,
+ * it will copy the file from `template/foo/bar/baz.ts.handlebars` to `new-app/foo/bar/baz.ts`,
+ * and use the {@link config} for whatever values are needed in the handlebars file.
+ *
+ * @param config The {@link FeatureConfig} to populate the handlebars file with.
+ * @param directory The directory to copy to.
+ * @param pathInTemplateDirectory The file in the {@link templateDirectory} to copy from.
+ * Expects the file to have a `.handlebars` extension.
+ */
+async function copyFromHandlebars(
+    config: FeaturesConfig,
+    directory: string,
+    pathInTemplateDirectory: string
+): Promise<void> {
+    const handlebarsFile: string = path.join(
+        templateDirectory,
+        pathInTemplateDirectory
+    );
+    // This should remove the `.handlebars` extension.
+    const parsedPath = path.parse(pathInTemplateDirectory);
+    const destination: string = path.join(
+        directory,
+        parsedPath.dir,
+        parsedPath.name
+    );
+    const destinationDirectory: string = path.dirname(destination);
+
+    const buffer: Buffer = await fs.promises.readFile(handlebarsFile);
+    const template = handlebars.compile(buffer.toString());
+    const content: string = template(config);
+    await fs.promises.mkdir(destinationDirectory, { recursive: true });
+    return fs.promises.writeFile(destination, content);
+}
 
 /**
  * Helper to make logging just a bit easier.
@@ -153,8 +234,104 @@ export class Command extends clipanion.Command {
             logDebug(`Downloaded ${this.example} example`);
         }
 
-        if (this.features?.length !== 0) {
+        if (this.features != null && this.features.length !== 0) {
             logInfo(`Initializing the following features: ${this.features}…`);
+            const config: FeaturesConfig = {
+                customWidget: this.features.includes('custom-widget'),
+                dashboardCogMenu: this.features.includes('dashboard-cog-menu'),
+                modal: this.features.includes('modal'),
+                sidePanel: this.features.includes('side-panel'),
+                widgetContextMenu: this.features.includes('widget-context-menu')
+            };
+
+            // Top-level files
+            await copyFromTemplate(directory, '.gitignore');
+            await copyFromTemplate(directory, '.prettierignore');
+            await copyFromTemplate(directory, 'package.json');
+            await copyFromTemplate(directory, 'README.md');
+            await copyFromTemplate(directory, 'tsconfig.json');
+
+            // `/public` files
+            await copyFromHandlebars(
+                config,
+                directory,
+                path.join('public', '_redirects.handlebars')
+            );
+            await copyFromTemplate(
+                directory,
+                path.join('public', 'favicon.ico')
+            );
+            await copyFromTemplate(
+                directory,
+                path.join('public', 'index.html')
+            );
+            await copyFromTemplate(
+                directory,
+                path.join('public', 'robots.txt')
+            );
+
+            // `/src` files
+            await copyFromTemplate(directory, path.join('src', 'index.css'));
+            await copyFromTemplate(
+                directory,
+                path.join('src', 'react-app-env.d.ts')
+            );
+            await copyFromHandlebars(
+                config,
+                directory,
+                path.join('src', 'index.tsx.handlebars')
+            );
+
+            // `/src/controller` files
+            await copyFromHandlebars(
+                config,
+                directory,
+                path.join('src', 'controller', 'index.ts.handlebars')
+            );
+
+            if (config.customWidget) {
+                await copyFromTemplate(
+                    directory,
+                    path.join('src', 'custom-widget', 'custom-widget.css')
+                );
+                await copyFromTemplate(
+                    directory,
+                    path.join('src', 'custom-widget', 'index.tsx')
+                );
+            }
+
+            if (config.dashboardCogMenu) {
+                await copyFromTemplate(
+                    directory,
+                    path.join('src', 'controller', 'dashboard-cog-menu.ts')
+                );
+            }
+
+            if (config.modal) {
+                await copyFromTemplate(
+                    directory,
+                    path.join('src', 'controller', 'modal.ts')
+                );
+                await copyFromTemplate(
+                    directory,
+                    path.join('src', 'modal', 'index.tsx')
+                );
+            }
+
+            if (config.sidePanel) {
+                await copyFromTemplate(
+                    directory,
+                    path.join('src', 'side-panel', 'index.tsx')
+                );
+            }
+
+            if (config.widgetContextMenu) {
+                await copyFromTemplate(
+                    directory,
+                    path.join('src', 'controller', 'widget-context-menu.ts')
+                );
+            }
+
             logDebug(`Initialized the following features: ${this.features}`);
         }
 
