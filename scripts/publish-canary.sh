2165b9064edf27646b517713d70a35acd71a4b2c
Hardy Jones <88002054+joneshf-dd@users.noreply.github.com>
2022-02-11 10:13:06 -0800

    UIAPPS-136 Don't deal with tags

--- scripts/publish-canary.sh
+++ scripts/publish-canary.sh
@@ -8,5 +8,4 @@ IFS=$'\n\t'
 readonly SHA=$(git rev-parse --short HEAD)
 readonly SNAPSHOT="canary.${SHA}"
 yarn changeset version --snapshot "${SNAPSHOT}"
-yarn changeset publish --tag canary
-git tag --contains HEAD --list --format='yarn add %(refname:strip=2)'
+yarn changeset publish --no-git-tag --tag canary
