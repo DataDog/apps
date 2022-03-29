6974631462aca0b93deb95b7e4e857ae7ba82f46
tdimnet <thomas.dimnet@datadoghq.com>
2022-03-29 15:13:22 +0200

    Add all API routes

--- examples/slice-pizza/proxy/app.py
+++ examples/slice-pizza/proxy/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 CORS(app)
 
 API_URL = 'http://api:3000/api'
-TOKEN = 'EewtzQMfhSnbt3AwY98EQB'
+TOKEN = 'b10WW0pmL45BlNl8nfCKTP'
 
 @app.route('/api/users', methods=['POST'])
 def create_user():
@@ -70,3 +70,58 @@ def get_menu():
     }
 
     return jsonify(res)
+
+
+@app.route('/api/cart', methods=['GET'])
+def get_cart():
+    url = '{}/cart?email=thomas.dimnet@datadoghq.com'.format(API_URL)
+
+    payload={}
+    headers = {
+      'token': TOKEN
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    return response.text
+
+
+
+@app.route('/api/cart', methods=['POST'])
+def update_cart():
+    url = '{}/cart'.format(API_URL)
+
+    payload = json.dumps({
+      "email": "thomas.dimnet@datadoghq.com",
+      "id": "da8e-9f48-eac6",
+      "size": 10,
+      "amount": 1
+    })
+    headers = {
+      'token': TOKEN,
+      'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    return response.text
+
+
+@app.route('/api/order', methods=['POST'])
+def create_order():
+    url = "{}/order".format(API_URL)
+
+    payload = json.dumps({
+      "email": "thomas.dimnet@datadoghq.com"
+    })
+    headers = {
+      'token': TOKEN,
+      'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    print(response.text)
+
+    return "ok"
+
