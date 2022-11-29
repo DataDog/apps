3c8bac9b4089a1e66eaf9b599e09fbcf6ba14eba
Jarrett Gossett <jarrettgossett@gmail.com>
2022-11-29 10:32:13 -0500

    Added esm target ouput for ui extensions sdk

--- packages/ui-extensions-sdk/webpack.config.js
+++ packages/ui-extensions-sdk/webpack.config.js
@@ -1,46 +1,53 @@
 const path = require('path');
 const webpack = require('webpack');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const packageJson = require('./package.json');
 
-module.exports = (env, options) => {
-    const config = {
-        devtool: 'source-map',
-        target: ['web', 'es5'],
-        entry: './src/index.ts',
-        output: {
-            filename: 'ui-extensions-sdk.min.js',
-            path: path.resolve(__dirname, 'dist'),
-            library: 'DD_SDK',
-            libraryTarget: 'umd'
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.tsx?$/,
-                    loader: 'ts-loader'
-                }
-            ]
-        },
-        plugins: [
-            new webpack.DefinePlugin({
-                SDK_VERSION: JSON.stringify(packageJson.version)
-            })
-        ],
-        resolve: {
-            extensions: ['.ts', '.tsx', '.js']
-        },
-        devServer: {
-            clientLogLevel: 'warning',
-            open: true,
-            historyApiFallback: true,
-            stats: 'errors-only'
-        }
-    };
+const config = {
+    devtool: 'source-map',
+    entry: './src/index.ts',
 
-    if (options.mode === 'production') {
-        config.plugins.push(new CleanWebpackPlugin());
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader'
+            }
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            SDK_VERSION: JSON.stringify(packageJson.version)
+        })
+    ],
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    devServer: {
+        clientLogLevel: 'warning',
+        open: true,
+        historyApiFallback: true,
+        stats: 'errors-only'
     }
-
-    return config;
 };
+
+const umdConfig = {
+    ...config,
+    target: ['web', 'es5'],
+    entry: './src/index.ts',
+    output: {
+        filename: 'ui-extensions-sdk.min.js',
+        path: path.resolve(__dirname, 'dist'),
+        library: 'DD_SDK',
+        libraryTarget: 'umd'
+    }
+};
+
+const esmConfig = {
+    ...config,
+    output: {
+        filename: 'ui-extensions-sdk-esm.min.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+};
+
+module.exports = [umdConfig, esmConfig];
